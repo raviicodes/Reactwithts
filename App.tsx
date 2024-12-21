d960b3e4ed106b97c93ace2881612f04eb9f7d20
Ravikant Yadav <101169995+raviicodes@users.noreply.github.com>
2024-12-21 17:23:23 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
  import { BrowserRouter,Routes,Route } from "react-router-dom";
  import Problems from "./components/Problems";
+import Navbar from "./components/Navbar";
+import Problem from "./components/Problem";
+import Contest from "./components/Contest";
 function App() {
 
   return <>
    <BrowserRouter>
+    <Navbar/>
      <Routes>
        <Route path="/" element={<Problems/>}/>
+       <Route path="/problem/:pid" element={<Problem/>}/>
+       <Route path="/contests" element={<Contest/>}/>
      </Routes>
    </BrowserRouter>
   </>;
